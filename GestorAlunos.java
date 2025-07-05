ac7cf77866de50f3c339c1bfc9003d566fab2890
joazis-quebra-codegos <joao.ds.zantti@gmail.com>
2025-07-05 01:15:19 -0300

    Update and rename main.java to GestorAlunos.java
    Main que roda as funções da classe aluno

---
+++ GestorAlunos.java
@@ -0,0 +1,27 @@
+package JovemAprendiz;
+
+import javax.swing.JOptionPane;
+
+public class GestorAlunos {
+    static public String[] MATERIAS = {"Matemática","Português","História","Geografia","Inglês","Ciências"};
+    public static void main(String[] args) {
+        aluno al = new aluno(null, null);
+        String[] opcoes = {"Cadastrar", "Adicionar Notas", "Mostrar Notas", "Sair"};
+        while (true) {
+            String escolha = (String) JOptionPane.showInputDialog(
+                null, "O que deseja fazer?", "Menu",
+                JOptionPane.PLAIN_MESSAGE, null, opcoes, opcoes[0]
+            );
+            if (escolha == null || escolha.equals("Sair")) {
+                break;
+            }
+            if (escolha.equals("Cadastrar")) {
+                al.cadastrar();
+            } else if (escolha.equals("Adicionar Notas")) {
+                al.adicionarNotas();
+            } else if (escolha.equals("Mostrar Notas")) {
+                al.mostrarNotas();
+            }
+        }
+    }
+}
